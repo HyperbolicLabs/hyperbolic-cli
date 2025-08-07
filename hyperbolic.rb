b24fca42acdfe4ec0a79b6a7a940927a29abf4d9
Am64r <theamrelhady@gmail.com>
2025-08-07 13:11:10 -0700

    update formula

--- hyperbolic.rb
+++ hyperbolic.rb
@@ -1,8 +1,8 @@
 class Hyperbolic < Formula
   desc "Command-line interface for managing GPU instances on Hyperbolic"
   homepage "https://github.com/HyperbolicLabs/hyperbolic-cli"
-  url "https://github.com/HyperbolicLabs/hyperbolic-cli/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "e518fe7843b627dec73cfa31c7c1b0e7dfff0d7b1054f9abef10ed27b0e5715d"
+  url "https://github.com/HyperbolicLabs/hyperbolic-cli/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "c6c88cbd0f48d67df9688a50b59b13fc3bd6b24cd4274e142f63d7fd1d8b7928"
   license "MIT"
   head "https://github.com/HyperbolicLabs/hyperbolic-cli.git", branch: "main"
 
